1ce6038f3d7ce71b5fcdd36a71faaaa431b30ea9
imamslamet810@gmail.com <imamslamet810@gmail.com>
2019-10-29 01:22:26 +0700

    finishing

--- js/main.js
+++ js/main.js
@@ -4,8 +4,8 @@ $(document).ready(function() {
         $(window).load(function() {
             $("#preloader").fadeOut("slow", function() {
                 $(this).remove();
-                $('.cover__title').velocity({ opacity: 1 }, { delay: 1000, duration: 1000 });
-                $('.cover__figure__name--1').velocity({ opacity: 1 }, { delay: 900, duration: 700 });
+                $(".cover__title").velocity({ opacity: 1 }, { delay: 1000, duration: 1000 });
+                $(".cover__figure__name--1").velocity({ opacity: 1 }, { delay: 900, duration: 700 });
             });
         })
     );
@@ -75,6 +75,13 @@ $(document).ready(function() {
     $(".modal1__close").click(function() {
         $(".frameModal01").fadeOut(300);
     });
+    $(".right__click011").click(function() {
+        $(".frameModal011").fadeIn(300);
+    });
+
+    $(".modal1__close").click(function() {
+        $(".frameModal011").fadeOut(300);
+    });
     // 02
     $(".right__click02").click(function() {
         $(".frameModal02").fadeIn(300);
@@ -82,6 +89,13 @@ $(document).ready(function() {
 
     $(".modal1__close").click(function() {
         $(".frameModal02").fadeOut(300);
+    });
+    $(".right__click021").click(function() {
+        $(".frameModal021").fadeIn(300);
+    });
+
+    $(".modal1__close").click(function() {
+        $(".frameModal021").fadeOut(300);
     });
     // 03
     $(".right__click03").click(function() {
@@ -91,6 +105,13 @@ $(document).ready(function() {
     $(".modal1__close").click(function() {
         $(".frameModal03").fadeOut(300);
     });
+    $(".right__click031").click(function() {
+        $(".frameModal031").fadeIn(300);
+    });
+
+    $(".modal1__close").click(function() {
+        $(".frameModal031").fadeOut(300);
+    });
     // 04
     $(".right__click04").click(function() {
         $(".frameModal04").fadeIn(300);
@@ -98,6 +119,13 @@ $(document).ready(function() {
 
     $(".modal1__close").click(function() {
         $(".frameModal04").fadeOut(300);
+    });
+    $(".right__click041").click(function() {
+        $(".frameModal041").fadeIn(300);
+    });
+
+    $(".modal1__close").click(function() {
+        $(".frameModal041").fadeOut(300);
     });
     // 05
     $(".right__click05").click(function() {
@@ -107,6 +135,13 @@ $(document).ready(function() {
     $(".modal1__close").click(function() {
         $(".frameModal05").fadeOut(300);
     });
+    $(".right__click051").click(function() {
+        $(".frameModal051").fadeIn(300);
+    });
+
+    $(".modal1__close").click(function() {
+        $(".frameModal051").fadeOut(300);
+    });
     // 06
     $(".right__click06").click(function() {
         $(".frameModal06").fadeIn(300);
@@ -114,6 +149,13 @@ $(document).ready(function() {
 
     $(".modal1__close").click(function() {
         $(".frameModal06").fadeOut(300);
+    });
+    $(".right__click061").click(function() {
+        $(".frameModal061").fadeIn(300);
+    });
+
+    $(".modal1__close").click(function() {
+        $(".frameModal061").fadeOut(300);
     });
     // 07
     $(".right__click07").click(function() {
@@ -123,6 +165,13 @@ $(document).ready(function() {
     $(".modal1__close").click(function() {
         $(".frameModal07").fadeOut(300);
     });
+    $(".right__click071").click(function() {
+        $(".frameModal071").fadeIn(300);
+    });
+
+    $(".modal1__close").click(function() {
+        $(".frameModal071").fadeOut(300);
+    });
     // 08
     $(".right__click08").click(function() {
         $(".frameModal08").fadeIn(300);
@@ -130,6 +179,13 @@ $(document).ready(function() {
 
     $(".modal1__close").click(function() {
         $(".frameModal08").fadeOut(300);
+    });
+    $(".right__click081").click(function() {
+        $(".frameModal081").fadeIn(300);
+    });
+
+    $(".modal1__close").click(function() {
+        $(".frameModal081").fadeOut(300);
     });
     // 09
     $(".right__click09").click(function() {
@@ -139,6 +195,13 @@ $(document).ready(function() {
     $(".modal1__close").click(function() {
         $(".frameModal09").fadeOut(300);
     });
+    $(".right__click091").click(function() {
+        $(".frameModal091").fadeIn(300);
+    });
+
+    $(".modal1__close").click(function() {
+        $(".frameModal091").fadeOut(300);
+    });
     // 10
     $(".right__click10").click(function() {
         $(".frameModal10").fadeIn(300);
@@ -146,6 +209,13 @@ $(document).ready(function() {
 
     $(".modal1__close").click(function() {
         $(".frameModal10").fadeOut(300);
+    });
+    $(".right__click101").click(function() {
+        $(".frameModal101").fadeIn(300);
+    });
+
+    $(".modal1__close").click(function() {
+        $(".frameModal101").fadeOut(300);
     });
 
     // page3
@@ -173,28 +243,260 @@ $(document).ready(function() {
     $(".modal2__close").click(function() {
         $(".frameModal13").fadeOut(300);
     });
+    // item3 mobile
+    // 1
+    $(".right__click131").click(function() {
+        $(".frameModal131").fadeIn(300);
+    });
 
+    $(".modal3__close").click(function() {
+        $(".frameModal131").fadeOut(300);
+    });
+    // 2
+    $(".right__click132").click(function() {
+        $(".frameModal132").fadeIn(300);
+    });
 
+    $(".modal3__close").click(function() {
+        $(".frameModal132").fadeOut(300);
+    });
+    // 3
+    $(".right__click133").click(function() {
+        $(".frameModal133").fadeIn(300);
+    });
+
+    $(".modal3__close").click(function() {
+        $(".frameModal133").fadeOut(300);
+    });
+    // 4
+    $(".right__click134").click(function() {
+        $(".frameModal134").fadeIn(300);
+    });
+
+    $(".modal3__close").click(function() {
+        $(".frameModal134").fadeOut(300);
+    });
+    // 5
+    $(".right__click135").click(function() {
+        $(".frameModal135").fadeIn(300);
+    });
+
+    $(".modal3__close").click(function() {
+        $(".frameModal135").fadeOut(300);
+    });
+
+    // 6
+    $(".right__click136").click(function() {
+        $(".frameModal136").fadeIn(300);
+    });
+
+    $(".modal3__close").click(function() {
+        $(".frameModal136").fadeOut(300);
+    });
+    // 7
+    $(".right__click137").click(function() {
+        $(".frameModal137").fadeIn(300);
+    });
+
+    $(".modal3__close").click(function() {
+        $(".frameModal137").fadeOut(300);
+    });
+
+    // back to top
+    $(window).scroll(function() {
+        if ($(this).scrollTop() >= 200) {
+            $(".backtop").fadeIn(500);
+        } else {
+            $(".backtop").fadeOut(500);
+        }
+    });
+    $(".backtop").click(function() {
+        $("body,html").animate({
+                scrollTop: 0
+            },
+            750
+        );
+    });
+
+    // page1
+    $(".right__click").click(function() {
+        $(".bg6__content--1--panah").hide();
+    });
+
+    $(".right__click1").click(function() {
+        $(".bg6__content--1--panah").hide();
+    });
+
+    $(".right__click2").click(function() {
+        $(".bg6__content--1--panah").hide();
+    });
+
+    $(".right__click3").click(function() {
+        $(".bg6__content--1--panah").hide();
+    });
+
+    $(".right__click4").click(function() {
+        $(".bg6__content--1--panah").hide();
+    });
+
+    $(".right__click5").click(function() {
+        $(".bg6__content--1--panah").hide();
+    });
+
+    $(".right__click6").click(function() {
+        $(".bg6__content--1--panah").hide();
+    });
+
+    //page3
+    $(".section1__canvas--1--01").click(function() {
+        $(".section1__canvas--1--panah").hide();
+    });
+    $(".section1__canvas--2--01").click(function() {
+        $(".section1__canvas--1--panah").hide();
+    });
+    $(".section1__canvas--3--01").click(function() {
+        $(".section1__canvas--1--panah").hide();
+    });
+    $(".section1__canvas--4--01").click(function() {
+        $(".section1__canvas--1--panah").hide();
+    });
+    $(".section1__canvas--5--01").click(function() {
+        $(".section1__canvas--1--panah").hide();
+    });
+    $(".section1__canvas--6--01").click(function() {
+        $(".section1__canvas--1--panah").hide();
+    });
+    $(".section1__canvas--7--01").click(function() {
+        $(".section1__canvas--1--panah").hide();
+    });
+
+    // page 3
+    $(".right__click11").click(function() {
+        $(".section6__left--1").hide();
+    });
+    $(".right__click12").click(function() {
+        $(".section6__left--1").hide();
+    });
+    $(".right__click13").click(function() {
+        $(".section6__left--1").hide();
+    });
+
+    // page3
+    $(".right__click131").click(function() {
+        $(".section1mobile__canvas--1--01--a").hide();
+    });
+    $(".right__click132").click(function() {
+        $(".section1mobile__canvas--1--01--a").hide();
+    });
+    $(".right__click133").click(function() {
+        $(".section1mobile__canvas--1--01--a").hide();
+    });
+    $(".right__click134").click(function() {
+        $(".section1mobile__canvas--1--01--a").hide();
+    });
+    $(".right__click135").click(function() {
+        $(".section1mobile__canvas--1--01--a").hide();
+    });
+    $(".right__click136").click(function() {
+        $(".section1mobile__canvas--1--01--a").hide();
+    });
+    $(".right__click137").click(function() {
+        $(".section1mobile__canvas--1--01--a").hide();
+    });
+
+    // page3
+    $(".right__click01").click(function() {
+        $(".bgdoc7__question--panah").hide();
+    });
+    $(".right__click011").click(function() {
+        $(".bgdoc7__question--panah").hide();
+    });
+
+    $(".right__click02").click(function() {
+        $(".bgdoc7__question--panah").hide();
+    });
+    $(".right__click021").click(function() {
+        $(".bgdoc7__question--panah").hide();
+    });
+
+    $(".right__click03").click(function() {
+        $(".bgdoc7__question--panah").hide();
+    });
+    $(".right__click031").click(function() {
+        $(".bgdoc7__question--panah").hide();
+    });
+
+    $(".right__click04").click(function() {
+        $(".bgdoc7__question--panah").hide();
+    });
+    $(".right__click041").click(function() {
+        $(".bgdoc7__question--panah").hide();
+    });
+
+    $(".right__click05").click(function() {
+        $(".bgdoc7__question--panah").hide();
+    });
+    $(".right__click051").click(function() {
+        $(".bgdoc7__question--panah").hide();
+    });
+
+    $(".right__click06").click(function() {
+        $(".bgdoc7__question--panah").hide();
+    });
+    $(".right__click061").click(function() {
+        $(".bgdoc7__question--panah").hide();
+    });
+
+    $(".right__click07").click(function() {
+        $(".bgdoc7__question--panah").hide();
+    });
+    $(".right__click071").click(function() {
+        $(".bgdoc7__question--panah").hide();
+    });
+
+    $(".right__click08").click(function() {
+        $(".bgdoc7__question--panah").hide();
+    });
+    $(".right__click081").click(function() {
+        $(".bgdoc7__question--panah").hide();
+    });
+
+    $(".right__click09").click(function() {
+        $(".bgdoc7__question--panah").hide();
+    });
+    $(".right__click091").click(function() {
+        $(".bgdoc7__question--panah").hide();
+    });
+
+    $(".right__click10").click(function() {
+        $(".bgdoc7__question--panah").hide();
+    });
+    $(".right__click101").click(function() {
+        $(".bgdoc7__question--panah").hide();
+    });
 
     // Scroll Reveal
     window.sr = ScrollReveal();
-    sr.reveal('.lazyloaded', {
+    sr.reveal(".lazyloaded", {
         duration: 500,
         delay: 100
     });
-    sr.reveal('.lazyloaded1', {
+    sr.reveal(".lazyloaded1", {
         duration: 1000,
         delay: 500
     });
-    sr.reveal('.lazyloaded2', {
+    sr.reveal(".lazyloaded2", {
         duration: 1000,
         delay: 500
     });
-    // 
-
+    //
 });
 // Mobile Version
-if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+if (
+    /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+        navigator.userAgent
+    )
+) {
     $(".cover__background").remove();
 } else {
     $(".social__item--wa").hide();
